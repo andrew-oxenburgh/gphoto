1377c0a1281c4a4ce9b8c9b64c0d6f0557a3e3d8
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2024-01-15 13:40:14 +1300

    finished, kind of

--- all.sh
+++ all.sh
@@ -1,8 +1,9 @@
 #/!/bin/bash
 
-echo "do you want to download the latest? (y/n)"
-read LATEST
-echo It\'s nice to meet you $LATEST
+# This is VERY rough little script.
+# There are false positives, so be aware.
+# gPhotos sometimes mangles the urls if there are 2 underscores in the query
+# I hate bash scripts, but here we are
 
 DIR=./calc
 DIR_DL=$DIR/download
@@ -15,7 +16,7 @@ ALBUM_PHOTOS=album-photos
 ALBUM_LIST=$DIR_2/$ALBUM_PHOTOS.txt
 ALL_LIST=$DIR_2/$ALL_PHOTOS.txt
 
-UNALBUMED_LIST=$DIR/unalbumed-files
+UNALBUMED_LIST=./unalbumed-files
 
 GOOGLE_PHOTO_SEARCH=https://photos.google.com/search/
 
@@ -23,61 +24,77 @@ mkdir -p $DIR_DL/
 mkdir -p $DIR_1/
 mkdir -p $DIR_2/
 
-download(){
-    echo "don't be impatient. This takes a while..."
-    rm $DIR_DL/*
+rm -f $UNALBUMED_LIST.md
 
-    # list all albums
-    # {"Path":"17 February 2013","Name":"17 February 2013","Size":-1,"MimeType":"inode/directory","ModTime":"2024-01-14T12:12:00+13:00","IsDir":true,"ID":"AHcsUy2nWqey-I0dC15otspu0JK2Z8rqVo0bBwK9rA-hdB_8gmG35MI6iKpHxzQBazzw5nYlhw-s"},
-    # rclone lsjson -R gphoto:album > $DIR_DL/album-names.json
+download() {
+    echo "don't be impatient. This takes a while. Like a couple of minutes..."
+    rm -f $DIR_DL/*
 
     # all photos in albums
-    rclone lsjson -R gphoto:album/ > $DIR_DL/$ALBUM_PHOTOS.json
+    rclone lsjson -R gphoto:album/ >$DIR_DL/$ALBUM_PHOTOS.json
 
     # list all photos anywhere
     # {"Path":"20200429_090441.jpg","Name":"20200429_090441.jpg","Size":-1,"MimeType":"image/jpeg","ModTime":"2020-04-28T21:04:40Z","IsDir":false,"ID":"AHcsUy0oaBwZGmfIaLLCd05mIioZv-WPkKVIV4QnJnnxKmJwK6rDq4MWv6MDwR9RRAlP-PZ7IzAddXsdGlpERAid6GZbOe-ecA"},
-    rclone lsjson -R gphoto:media/all > $DIR_DL/$ALL_PHOTOS.json
+    rclone lsjson -R gphoto:media/all >$DIR_DL/$ALL_PHOTOS.json
 }
 
-step_1 (){
+step_1() {
     rm -f $DIR_1/*
 
-    cat $DIR_DL/$ALL_PHOTOS.json | jp [*].Name  > $DIR_1/$ALL_PHOTOS.txt
-    cat $DIR_DL/$ALBUM_PHOTOS.json | jp [*].Name > $DIR_1/$ALBUM_PHOTOS.txt
+    cat $DIR_DL/$ALL_PHOTOS.json | jp [*].Name >$DIR_1/$ALL_PHOTOS.txt
+    cat $DIR_DL/$ALBUM_PHOTOS.json | jp [*].Name >$DIR_1/$ALBUM_PHOTOS.txt
 }
 
-step_2 (){
+step_2() {
     rm -f $DIR_2/*
 
-    cat $DIR_1/$ALBUM_PHOTOS.txt | gsed 's/^...//' | gsed 's/[,"]*$//' | sort -u > $DIR_2/$ALBUM_PHOTOS.txt
-
-    cat $DIR_1/$ALL_PHOTOS.txt | gsed 's/^...//' | gsed 's/[,"]*$//' | sort -u > $DIR_2/$ALL_PHOTOS.txt
-
-    comm -23 $ALL_LIST $ALBUM_LIST   > $DIR_2/UNALBUMMED.txt
-
-    gsed 's/^/https:\/\/photos.google.com\/search\//' $DIR_2/UNALBUMMED.txt > $UNALBUMED_LIST.md
+    cat $DIR_1/$ALBUM_PHOTOS.txt | gsed 's/^...//' | gsed 's/[,"]*$//' | sort -u >$DIR_2/$ALBUM_PHOTOS.txt
+    cat $DIR_1/$ALL_PHOTOS.txt | gsed 's/^...//' | gsed 's/[,"]*$//' | sort -u >$DIR_2/$ALL_PHOTOS.txt
+    comm -23 $ALL_LIST $ALBUM_LIST >$DIR_2/UNALBUMMED.txt
+    gsed 's/^/https:\/\/photos.google.com\/search\//' $DIR_2/UNALBUMMED.txt >$UNALBUMED_LIST.md
     wc -l $UNALBUMED_LIST.md
 }
 
-rm -f $UNALBUMED_LIST.md
+help() {
+    echo "Program: Creates a pretty rough list of urls of Google photos not in albums"
+    echo "Output: a markdown file with urls to photos not in albums"
+    echo "Requires: rclone (https://rclone.org/) to be installed and configured"
+    echo "Requires: jp (https://github.com/jmespath/jp) to be installed"
+    echo "Warning: Download will take a little while"
+    echo "Usage: [options]"
+    echo "Options:"
+    echo "  -h, --help           Display this help message"
+    echo "  -n, --no_download    don't do download"
+}
 
-if [ "$LATEST" = "y" ]
-then
-    download
-    step_1
-    step_2
-else
-    step_1
-    step_2
+NO_DOWNLOAD=DOWNLOAD
+HELP=NO_HELP
+
+for i in "$@"; do
+    case $i in
+    --no_download | -n)
+        NO_DOWNLOAD=NO_DOWNLOAD
+        shift # past argument=value
+        ;;
+    --help | -h)
+        HELP=HELP
+        shift # past argument=value
+        ;;
+    *) ;;
+    esac
+done
+
+# echo $HELP
+# echo $NO_DOWNLOAD
+
+if [ "$HELP" = "HELP" ]; then
+    help
+    exit
 fi
-exit
 
-# sed 's/^...\(.*\)...$/\1/' file.txt
+if [ "$NO_DOWNLOAD" = "DOWNLOAD" ]; then
+    download
+fi
 
-# awk '{gsub(/^[[:space:]]*/, ""); $0 = substr($0, 4); gsub(/[",]+$/, ""); print}' file.txt > output.txt
-
-# comm -23 <(sort file1.txt) <(sort file2.txt)
-
-# echo '  "02022009012.jpg",' | grep -o '[^",]*' | tr -d ' '
-
-# cat gsed 's/^...//' 's/["',]*$//' file.txt > output.txt
+step_1
+step_2
